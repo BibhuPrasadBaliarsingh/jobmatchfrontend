2718fd6a27f580ce9ab4ac71b5a5edd945c74715
BibhuPrasadBaliarsingh <bibhuprasadbaliarsingh@gmail.com>
2026-04-20 11:25:11 +0530

    update change

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const BACKEND_URL = 'https://jobmatchbackend-okz8.onrender.com/api';
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || '/api',
+  baseURL: BACKEND_URL,
   timeout: 15000,
   headers: { 'Content-Type': 'application/json' },
 });
